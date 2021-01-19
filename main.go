1bd84a1af7bad85d40337438fb8589965e0339ca
eunsukcho <hysces97@gmail.com>
2021-01-19 13:30:59 +0900

    url checker 최적화 전

--- main.go
+++ main.go
@@ -1,81 +1,46 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"learngo/myDict"
+	"net/http"
 )
 
+var errRequestFailed = errors.New("Request Failed")
+
 func main() {
-	dictionary := myDict.Dictionary{"first": "First word"}
+	var results = make(map[string]string)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
 
-	// Search
-	/*
-		definition, err := dictionary.Search("first")
-
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
 		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println(definition)
+			result = "FAILED"
 		}
-	*/
-
-	// Add
-	/*
-		word := "hello"
-		definition := "Greeting"
-		err := dictionary.Add(word, definition)
-		if err != nil {
-			fmt.Println(err)
-		}
-		h_definition, _ := dictionary.Search("hello")
-		fmt.Println(h_definition)
-
-		word2 := "hello"
-		definition2 := "Greeting"
-		err2 := dictionary.Add(word2, definition2)
-		if err2 != nil {
-			fmt.Println(err2)
-		}
-		h_definition2, _ := dictionary.Search("hello")
-		fmt.Println(h_definition2)
-	*/
-
-	// Update
-	/*
-		baseWord := "hello"
-		add_err := dictionary.Add(baseWord, "First")
-		if add_err != nil {
-			fmt.Println(add_err)
-		}
-		word, _ := dictionary.Search(baseWord)
-		fmt.Println("before update", word)
-
-		up_err := dictionary.Update("hello2", "Second")
-		if up_err != nil {
-			fmt.Println(up_err)
-		}
-		word2, _ := dictionary.Search(baseWord)
-		fmt.Println("after update", word2)
-	*/
-
-	// Delete
-	add_Word := "new Word"
-	add_err := dictionary.Add(add_Word, "newContent")
-	if add_err != nil {
-		fmt.Println(add_err)
+		results[url] = result
 	}
-	searchVal, _ := dictionary.Search(add_Word)
-	fmt.Println("Search Value", searchVal)
-
-	del_err := dictionary.Delete("add_Word")
-	if del_err != nil {
-		fmt.Println(del_err)
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
+}
 
-	searchVal2, searchErr := dictionary.Search(add_Word)
-	if searchErr != nil {
-		fmt.Println(searchErr)
-	} else {
-		fmt.Println("Search Value", searchVal2)
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println("Err Reason", err, resp.StatusCode)
+		return errRequestFailed
 	}
+	return nil
 }
